e9be48820f78875b7d56990c181a0f1fe3ec5452
IamXacer <phanton7487@gmail.com>
2022-10-26 02:20:52 +0300

    Skills

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import Header from "./Header/Header";
+import Main from "./main/Main";
+import Skills from "./skills/Skills";
 
 function App() {
-  return (
-    <div className="App">
-<Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+        </div>
+    );
 }
 
 export default App;
